0be23e4e8f3eb4ccad76e8e3d7d96da2ff9db424
Hagil Baik <hagilbaik@gmail.com>
2017-11-21 11:19:23 +0800

    The party adjourned to a hot tub, yes.  Fully clothed, I might add. 		-- IBM employee, testifying in California State Supreme Court

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,1 +1,21 @@
 console.log('loaded frontend_app');
+angular.module('mean_demo', []).controller('crudController', do_crud);
+
+function do_crud($scope, $http) {
+    console.log('inside do_crud');
+
+    $scope.read = function () {
+        console.log('reading all records');
+    }
+    $scope.create = function () {
+        console.log('creating user');
+    }
+    $scope.update = function (user) {
+        console.log('updating user');
+        console.log(user);
+    }
+    $scope.delete = function (user) {
+        console.log('deleting user');
+        console.log(user);
+    }
+}
